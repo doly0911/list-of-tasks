7887ae2d166661cb834889fa28e2677d1420171f
Doly Jimenez <doly.jimenez11@gmail.com>
2022-02-03 17:50:42 -0500

    Add css styles and new component Todo.js with its props

--- src/App.js
+++ src/App.js
@@ -1,97 +1,104 @@
+import Todo from "./components/Todo";
 
 function App() {
   return (
-    <div >
+    <div className="todoapp stack-large" >
       <h1>Aprendiendo React :)</h1>
 
       <form>
-        <h2>
-          <label> Qué necesitas hacer hoy?</label>
+        <h2 className="label-wrapper">
+          <label htmlFor="new-todo-input" className="label__lg">
+            Qué necesitas hacer hoy?
+          </label>
         </h2>
         <input
         type="text"
         id="new-todo-input"
+        className="input input__lg"
         name='text'
         autoComplete='off'
         />
-        <button type="submit">
+        <button type="submit" className="btn btn__primary btn__lg">
           Agregar
         </button>        
       </form> 
 
-      <div>
-        <button type="button">
-          <span>Show</span>
+      <div className="filters btn-group stack-exception">
+        <button type="button" className="btn toggle-btn" aria-pressed="true">
+          <span className="visually-hidden">Show</span>
           <span> all</span>
-          <span> tasks</span>
+          <span className="visually-hidden"> tasks</span>
         </button>
-        <button>
-          <span>Show</span>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show</span>
           <span> Active</span>
-          <span> tasks</span>
+          <span className="visually-hidden"> tasks</span>
         </button>
-        <button>
-          <span>Show</span>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show</span>
           <span> Completed</span>
-          <span> tasks</span>
+          <span className="visually-hidden"> tasks</span>
         </button>
       </div>
 
-      <h2>
+      <h2 id="list-heading">
         3 tasks remaining
       </h2>
 
-      <ul>
-        <li>
-          <div>
-          <input/>
-          <label>
+      <ul
+      role="list"
+      className="todo-list stack-large stack-exception"
+      aria-labelledby="list-heading"
+      >
+        <Todo name="Eat" completed={true} id="todo-0" className="todo stack-small">        
+          <div className="c-cb">
+          <input id="todo-0" type="checkbox" defaultChecked={true}/>
+          <label className="todo-label" htmlFor="todo-0">
             Eat
           </label>        
           </div>
-          <div>
-            <button>
-              Edit <span> Eat</span>
+          <div className="btn-group">
+            <button type="button" className="btn">
+              Edit <span className="visually-hidden"> Eat</span>
             </button>
-            <button>
-              Delete <span> Eat</span>
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden"> Eat</span>
             </button>
-          </div>
-        </li>
-
-        <li>
-          <div>
-          <input/>
-          <label>
+          </div>       
+        </Todo>
+        <Todo name="Sleep" completed={false} id="todo-1" className="todo stack-small">
+          <div className="c-cb">
+          <input id="todo-1" type="checkbox"/>
+          <label className="todo-label" htmlFor="todo-1">
             Sleep
           </label>        
           </div>
-          <div>
-            <button>
-              Edit <span> Sleep</span>
+          <div className="btn-group">
+            <button type="button" className="btn">
+              Edit <span className="visually-hidden"> Sleep</span>
             </button>
-            <button>
-              Delete <span> Sleep</span>
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden"> Sleep</span>
             </button>
           </div>
-        </li>
+          </Todo>
 
-        <li>
-          <div>
-          <input/>
-          <label>
+        <Todo name="Repeat" completed={false} id="todo-2" className="todo stack-small">
+          <div className="c-cb">
+          <input id="todo-2" type="checkbox"/>
+          <label className="todo-label" htmlFor="todo-2">
           Repeat
           </label>        
           </div>
-          <div>
-            <button>
-              Edit <span> Repeat</span>
+          <div className="btn-group">
+            <button type="button" className="btn">
+              Edit <span className="visually-hidden"> Repeat</span>
             </button>
-            <button>
-              Delete <span> Repeat</span>
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden"> Repeat</span>
             </button>
           </div>
-        </li>
+        </Todo>
       </ul>
 
     </div>
