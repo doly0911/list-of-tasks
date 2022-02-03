93cbd8e4ecafe8b61771dc24d39fd280999837c3
Doly Jimenez <doly.jimenez11@gmail.com>
2022-02-02 19:50:22 -0500

    Clean project and add the initial structure of the form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>Aprendiendo React :)</h1>
+
+      <form>
+        <h2>
+          <label> Qué necesitas hacer hoy?</label>
+        </h2>
+        <input
+        type="text"
+        id="new-todo-input"
+        name='text'
+        autoComplete='off'
+        />
+        <button type="submit">
+          Agregar
+        </button>        
+      </form> 
+
+      <div>
+        <button type="button">
+          <span>Show</span>
+          <span> all</span>
+          <span> tasks</span>
+        </button>
+        <button>
+          <span>Show</span>
+          <span> Active</span>
+          <span> tasks</span>
+        </button>
+        <button>
+          <span>Show</span>
+          <span> Completed</span>
+          <span> tasks</span>
+        </button>
+      </div>
+
+      <h2>
+        3 tasks remaining
+      </h2>
+
+      <ul>
+        <li>
+          <div>
+          <input/>
+          <label>
+            Eat
+          </label>        
+          </div>
+          <div>
+            <button>
+              Edit <span> Eat</span>
+            </button>
+            <button>
+              Delete <span> Eat</span>
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <div>
+          <input/>
+          <label>
+            Sleep
+          </label>        
+          </div>
+          <div>
+            <button>
+              Edit <span> Sleep</span>
+            </button>
+            <button>
+              Delete <span> Sleep</span>
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <div>
+          <input/>
+          <label>
+          Repeat
+          </label>        
+          </div>
+          <div>
+            <button>
+              Edit <span> Repeat</span>
+            </button>
+            <button>
+              Delete <span> Repeat</span>
+            </button>
+          </div>
+        </li>
+      </ul>
+
     </div>
   );
 }
