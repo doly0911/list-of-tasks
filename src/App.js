1317bafb446b34993627b0c3d969f9ea712293f9
Doly Jimenez <doly.jimenez11@gmail.com>
2022-02-09 18:43:08 -0500

    funcion eliminar tarea

--- src/App.js
+++ src/App.js
@@ -42,7 +42,10 @@ function App(props) {
   }
 
   function deleteTask(id){
-    console.log(id)
+    const tareasRestantes = tasks.filter(
+      task => id !== task.id
+    );
+    setTasks(tareasRestantes);
   }
 
   return (
