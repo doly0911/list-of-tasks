feb2fda50fbae9feec4db078835175d0b1ee308f
Doly Jimenez <doly.jimenez11@gmail.com>
2022-02-09 23:19:54 -0500

    funcion de filtrar las tareas

--- src/App.js
+++ src/App.js
@@ -4,10 +4,21 @@ import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import { nanoid } from "nanoid";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks)
+  const [filter, setFilter] = useState('All');
   
-  const taskList = tasks.map(task => 
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map(task => 
         <Todo 
             id={task.id}
             name={task.name}
@@ -18,6 +29,17 @@ function App(props) {
             editTask = {editTask}
         />
         );
+
+  const filterList = FILTER_NAMES.map(
+    name => (
+      <FilterButton 
+        key={name}
+        name={name} 
+        isPressed={name === filter}
+        setFilter = {setFilter}
+        />
+    )
+  );
 
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
@@ -66,9 +88,7 @@ function App(props) {
       <h1>Aprendiendo React :)</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton nameFilter="All"/>
-        <FilterButton nameFilter="Active"/>
-        <FilterButton nameFilter="Completed"/>
+        {filterList}
       </div>     
 
       <h2 id="list-heading">
@@ -76,7 +96,6 @@ function App(props) {
       </h2>
 
       <ul
-      role="list"
       className="todo-list stack-large stack-exception"
       aria-labelledby="list-heading">
         {taskList}
